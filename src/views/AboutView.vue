b60a6e212da4f99989bf314eaa83db122c0175f6
jmcob <77777490+jmcob@users.noreply.github.com>
2022-05-16 07:11:50 +0200

    about.png added

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,10 @@
   <div class="about">
     <h1>Coded by Jean-Maxime Cobac</h1>
     <p>in May 2022.</p>
+    <img alt="about us ipad image" src="../assets/about.png" />
+
     <p>C'est déja la troisième version de jmxi.art.</p>
   </div>
 </template>
+
+<style scoped></style>
