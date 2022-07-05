a068a7517729b53deb2152aae001622554a617da
jmcob <77777490+jmcob@users.noreply.github.com>
2022-07-05 06:49:05 +0200

    update pains de vie

--- src/views/BreadView.vue
+++ src/views/BreadView.vue
@@ -4,7 +4,7 @@
     <h1>Gros pains de Vie</h1>
     <div class="cadre">
       <p>
-        Vous trouverez ici de
+        Vous trouverez bientôt ici de
         <strong>gros pains de vie libres de droit</strong>, sélectionnés par
         notre équipe d'adorateurs du très saint sacrement. <br /><br />
 
@@ -16,11 +16,6 @@
         <br /><br />
         Il y a 83 pains issus de psaumes et 65 issus du reste de la Bible.
       </p>
-      <h2>
-        <a href="@/assets/pains/148-pains-de-vie.pdf"
-          >Télécharger 148 gros pains de Vie (PDF)</a
-        >
-      </h2>
     </div>
   </div>
 </template>
